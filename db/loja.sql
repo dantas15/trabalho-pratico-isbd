f72a60808af68567b9792b6ea13dfdc5570028d7
Gustavo Dantas Galote <gustav0d@protonmail.com>
2023-12-03 09:50:01 -0300

    style: formatar o arquivo sql principal

--- db/loja.sql
+++ db/loja.sql
@@ -17,14 +17,14 @@ CREATE SCHEMA IF NOT EXISTS `loja` DEFAULT CHARACTER SET utf8 ;
 -- -----------------------------------------------------
 -- Schema world
 -- -----------------------------------------------------
-USE `loja` ;
+USE `loja`;
 
 -- -----------------------------------------------------
 -- Table `loja`.`Cliente`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`Cliente` (
-                                                `idCliente` INT NOT NULL AUTO_INCREMENT,
-                                                `cpf` VARCHAR(11) NOT NULL,
+    `idCliente` INT NOT NULL AUTO_INCREMENT,
+    `cpf` VARCHAR(11) NOT NULL,
     `nomeCliente` VARCHAR(256) NOT NULL,
     `numTelefone` VARCHAR(11) NOT NULL,
     `logradouro` VARCHAR(255) NOT NULL,
@@ -35,106 +35,107 @@ CREATE TABLE IF NOT EXISTS `loja`.`Cliente` (
     `cep` VARCHAR(8) NOT NULL,
     `estado` VARCHAR(2) NOT NULL,
     PRIMARY KEY (`idCliente`),
-    UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC) VISIBLE)
-    ENGINE = InnoDB;
+    UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC)
+) ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
 -- Table `loja`.`Colaborador`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`Colaborador` (
-                                                    `idColaborador` INT NOT NULL AUTO_INCREMENT,
-                                                    `cargo` VARCHAR(45) NOT NULL,
+    `idColaborador` INT NOT NULL AUTO_INCREMENT,
+    `cargo` VARCHAR(45) NOT NULL,
     `dataContratação` DATE NOT NULL,
-    PRIMARY KEY (`idColaborador`))
-    ENGINE = InnoDB;
+    PRIMARY KEY (`idColaborador`)
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `loja`.`Pedido`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`Pedido` (
-                                               `idPedido` INT NOT NULL AUTO_INCREMENT,
-                                               `dataPedido` DATE NOT NULL,
-                                               `valorTotal` REAL(8,2) NOT NULL,
+    `idPedido` INT NOT NULL AUTO_INCREMENT,
+    `dataPedido` DATE NOT NULL,
+    `valorTotal` REAL(8,2) NOT NULL,
     `idCliente` INT NOT NULL,
     `idColaborador` INT NOT NULL,
     PRIMARY KEY (`idPedido`),
-    INDEX `fk_Pedido_Cliente_idx` (`idCliente` ASC) VISIBLE,
-    INDEX `fk_Pedido_Colaborador1_idx` (`idColaborador` ASC) VISIBLE,
+    INDEX `fk_Pedido_Cliente_idx` (`idCliente` ASC),
+    INDEX `fk_Pedido_Colaborador1_idx` (`idColaborador` ASC),
     CONSTRAINT `fk_Pedido_Cliente`
-    FOREIGN KEY (`idCliente`)
-    REFERENCES `loja`.`Cliente` (`idCliente`)
-    ON DELETE RESTRICT
-    ON UPDATE NO ACTION,
+        FOREIGN KEY (`idCliente`)
+        REFERENCES `loja`.`Cliente` (`idCliente`)
+        ON DELETE RESTRICT
+        ON UPDATE NO ACTION,
     CONSTRAINT `fk_Pedido_Colaborador1`
-    FOREIGN KEY (`idColaborador`)
-    REFERENCES `loja`.`Colaborador` (`idColaborador`)
-    ON DELETE RESTRICT
-    ON UPDATE NO ACTION)
-    ENGINE = InnoDB;
+        FOREIGN KEY (`idColaborador`)
+        REFERENCES `loja`.`Colaborador` (`idColaborador`)
+        ON DELETE RESTRICT
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `loja`.`Categoria`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`Categoria` (
-                                                  `idCategoria` INT NOT NULL AUTO_INCREMENT,
-                                                  `nomeCat` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`idCategoria`))
-    ENGINE = InnoDB;
+    `idCategoria` INT NOT NULL AUTO_INCREMENT,
+    `nomeCat` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`idCategoria`)
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `loja`.`Produto`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`Produto` (
-                                                `idProduto` INT NOT NULL AUTO_INCREMENT,
-                                                `nomeProd` VARCHAR(80) NOT NULL,
+    `idProduto` INT NOT NULL AUTO_INCREMENT,
+    `nomeProd` VARCHAR(80) NOT NULL,
     `descrição` VARCHAR(255) NOT NULL,
     `preço` REAL(8,2) NOT NULL,
     `quantEstoque` INT(6) NOT NULL,
     `idCategoria` INT NOT NULL,
     PRIMARY KEY (`idProduto`),
-    INDEX `fk_Produto_Categoria1_idx` (`idCategoria` ASC) VISIBLE,
+    INDEX `fk_Produto_Categoria1_idx` (`idCategoria` ASC),
     CONSTRAINT `fk_Produto_Categoria1`
-    FOREIGN KEY (`idCategoria`)
-    REFERENCES `loja`.`Categoria` (`idCategoria`)
-    ON DELETE RESTRICT
-    ON UPDATE NO ACTION)
-    ENGINE = InnoDB;
+        FOREIGN KEY (`idCategoria`)
+        REFERENCES `loja`.`Categoria` (`idCategoria`)
+        ON DELETE RESTRICT
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `loja`.`PedidoProduto`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`PedidoProduto` (
-                                                      `idPedido` INT NOT NULL,
-                                                      `idProduto` INT(3) NOT NULL,
+    `idPedido` INT NOT NULL,
+    `idProduto` INT(3) NOT NULL,
     `precoVenda` REAL(8,2) NOT NULL,
     `quantidade` INT(5) NOT NULL,
     PRIMARY KEY (`idPedido`, `idProduto`),
-    INDEX `fk_Pedido_has_Produto_Produto1_idx` (`idProduto` ASC) VISIBLE,
-    INDEX `fk_Pedido_has_Produto_Pedido1_idx` (`idPedido` ASC) VISIBLE,
+    INDEX `fk_Pedido_has_Produto_Produto1_idx` (`idProduto` ASC),
+    INDEX `fk_Pedido_has_Produto_Pedido1_idx` (`idPedido` ASC),
     CONSTRAINT `fk_Pedido_has_Produto_Pedido1`
-    FOREIGN KEY (`idPedido`)
-    REFERENCES `loja`.`Pedido` (`idPedido`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION,
+        FOREIGN KEY (`idPedido`)
+        REFERENCES `loja`.`Pedido` (`idPedido`)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION,
     CONSTRAINT `fk_Pedido_has_Produto_Produto1`
-    FOREIGN KEY (`idProduto`)
-    REFERENCES `loja`.`Produto` (`idProduto`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION)
-    ENGINE = InnoDB;
+        FOREIGN KEY (`idProduto`)
+        REFERENCES `loja`.`Produto` (`idProduto`)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `loja`.`Fornecedor`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`Fornecedor` (
-                                                   `idFornecedor` INT NOT NULL AUTO_INCREMENT,
-                                                   `nomeForn` VARCHAR(80) NOT NULL,
+    `idFornecedor` INT NOT NULL AUTO_INCREMENT,
+    `nomeForn` VARCHAR(80) NOT NULL,
     `numTelefone` VARCHAR(11) NOT NULL,
     `logradouro` VARCHAR(255) NOT NULL,
     `cidade` VARCHAR(45) NOT NULL,
@@ -143,29 +144,31 @@ CREATE TABLE IF NOT EXISTS `loja`.`Fornecedor` (
     `bairro` VARCHAR(45) NOT NULL,
     `cep` VARCHAR(8) NOT NULL,
     `estado` VARCHAR(2) NOT NULL,
-    PRIMARY KEY (`idFornecedor`))
-    ENGINE = InnoDB;
+    PRIMARY KEY (`idFornecedor`)
+) ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 -- Table `loja`.`Fornece`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `loja`.`Fornece` (
-                                                `idFornecedor` INT NOT NULL,
-                                                `idProduto` INT NOT NULL,
-                                                INDEX `fk_Fornecedor_has_Produto_Produto1_idx` (`idProduto` ASC) VISIBLE,
-    INDEX `fk_Fornecedor_has_Produto_Fornecedor1_idx` (`idFornecedor` ASC) VISIBLE,
+    `idFornecedor` INT NOT NULL,
+    `idProduto` INT NOT NULL,
+    INDEX `fk_Fornecedor_has_Produto_Produto1_idx` (`idProduto` ASC),
+    INDEX `fk_Fornecedor_has_Produto_Fornecedor1_idx` (`idFornecedor` ASC),
     PRIMARY KEY (`idFornecedor`, `idProduto`),
     CONSTRAINT `fk_Fornecedor_has_Produto_Fornecedor1`
-    FOREIGN KEY (`idFornecedor`)
-    REFERENCES `loja`.`Fornecedor` (`idFornecedor`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION,
+        FOREIGN KEY (`idFornecedor`)
+        REFERENCES `loja`.`Fornecedor` (`idFornecedor`)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION,
     CONSTRAINT `fk_Fornecedor_has_Produto_Produto1`
-    FOREIGN KEY (`idProduto`)
-    REFERENCES `loja`.`Produto` (`idProduto`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION)
-    ENGINE = InnoDB;
+        FOREIGN KEY (`idProduto`)
+        REFERENCES `loja`.`Produto` (`idProduto`)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
@@ -176,32 +179,32 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- *** Exemplos de Alter Table ***
 -- *******************************************************
 
-/*Adicionando uma nova coluna chamada 'email' à tabela 'Cliente'*/
+/* Adicionando uma nova coluna chamada 'email' à tabela 'Cliente' */
 ALTER TABLE `Cliente`
     ADD COLUMN `email` VARCHAR(100) AFTER `numTelefone`;
 
-/*Alterando o tipo da coluna 'numero' de INT para VARCHAR(10) na tabela 'Cliente'*/
+/* Alterando o tipo da coluna 'numero' de INT para VARCHAR(10) na tabela 'Cliente' */
 ALTER TABLE `Cliente`
     MODIFY COLUMN `numero` VARCHAR(10);
 
-/*Removendo a restrição UNIQUE da coluna 'cpf' na tabela 'Cliente'*/
+/* Removendo a restrição UNIQUE da coluna 'cpf' na tabela 'Cliente' */
 ALTER TABLE `Cliente`
-DROP INDEX `cpf_UNIQUE`;
+    DROP INDEX `cpf_UNIQUE`;
 
-/*Criando uma tabela extra chamada 'Exemplo'*/
+/* Criando uma tabela extra chamada 'Exemplo' */
 CREATE TABLE IF NOT EXISTS `Exemplo` (
-                                         `id` INT NOT NULL AUTO_INCREMENT,
-                                         `descricao` VARCHAR(255) NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `descricao` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
-    );
+);
 
-/*Inserindo alguns dados na tabela 'Exemplo'*/
+/* Inserindo alguns dados na tabela 'Exemplo' */
 INSERT INTO `Exemplo` (`descricao`) VALUES
-                                        ('Exemplo 1'),
-                                        ('Exemplo 2'),
-                                        ('Exemplo 3');
+('Exemplo 1'),
+('Exemplo 2'),
+('Exemplo 3');
 
-/*Removendo a tabela 'Exemplo'*/
+/* Removendo a tabela 'Exemplo' */
 DROP TABLE IF EXISTS `Exemplo`;
 
 -- *******************************************************
@@ -433,7 +436,6 @@ VALUES
 UPDATE `loja`.`Cliente`
 SET `nomeCliente` = 'Novo Nome'
 WHERE `idCliente` = 1;
-
 
 -- Atualizando o preço do produto com ID 2
 UPDATE `loja`.`Produto`
