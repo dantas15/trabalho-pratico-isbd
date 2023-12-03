8c93afdb5aae32e2755b3e5fa13451f42353b518
Gustavo Dantas Galote <gustav0d@protonmail.com>
2023-12-03 17:47:13 -0300

    chore(inicial): adicionar links para todos os recursos

--- pages/inicial.php
+++ pages/inicial.php
@@ -1,1 +1,39 @@
-<h1>Oiiiiiiiii</h1>
+<div>
+  <h1>Tela principal</h1>
+
+  <hr />
+
+  <h2>Escolha o que deseja gerenciar:</h2>
+  <div class="gerenciar-todos">
+    <div class="gerenciar">
+      <h3>Pedidos</h3>
+      <a href="/pedido/novo" class="gerenciar-botao adicionar">Novo pedido</a>
+      <a href="/" class="gerenciar-botao">Histórico de pedidos</a>
+    </div>
+    <div class="gerenciar">
+      <h3>Clientes</h3>
+      <a href="/" class="gerenciar-botao adicionar">Cadastrar cliente</a>
+      <a href="/" class="gerenciar-botao">Clientes cadastrados</a>
+    </div>
+    <div class="gerenciar">
+      <h3>Produtos</h3>
+      <a href="/" class="gerenciar-botao adicionar">Cadastrar produto</a>
+      <a href="/" class="gerenciar-botao">Produtos cadastrados</a>
+    </div>
+    <div class="gerenciar">
+      <h3>Categoria de produtos</h3>
+      <a href="/" class="gerenciar-botao">Listar categorias</a>
+      <a href="/" class="gerenciar-botao adicionar">Adicionar categoria</a>
+    </div>
+    <div class="gerenciar">
+      <h3>Fornecedores</h3>
+      <a href="/" class="gerenciar-botao">Listar fornecedores</a>
+      <a href="/" class="gerenciar-botao adicionar">Adicionar fornecedor</a>
+    </div>
+    <div class="gerenciar">
+      <h3>Colaboradores</h3>
+      <a href="/" class="gerenciar-botao">Listar colaboradores</a>
+      <a href="/" class="gerenciar-botao adicionar">Adicionar colaborador</a>
+    </div>
+  </div>
+</div>
