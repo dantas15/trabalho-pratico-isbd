8b3fd4321b46359c34d32a52199c598780efaef6
Gustavo Dantas Galote <gustav0d@protonmail.com>
2023-12-03 16:07:22 -0300

    feat(db): adicionar conexão php e mysqli

--- db/config.php
+++ db/config.php
@@ -1,5 +1,11 @@
 <?php
-$dbHost = "127.0.0.1";
-$dbLogin = "usario_padrao";
-$dbSenha = "senha123";
+/*
+  Estas configurações funcionam por padrão para rodar com o docker-compose.yml
+  Caso você queira rodar sem o docker:
+    - altere o $dbHost para 127.0.0.1
+*/
+$dbHost = "db";
+$dbLogin = "root";
+$dbSenha = "root";
 $dbNomeBanco = "loja";
+$dbPorta = 3306;
--- index.php
+++ index.php
@@ -5,6 +5,10 @@ $url = explode('?', $_SERVER['REQUEST_URI']);
 // incluir o mesmo head em todas as rotas
 include __DIR__ . '/includes/head.php';
 
+// iniciar conexão com o banco de dados
+include('db/config.php');
+$con = mysqli_connect($dbHost, $dbLogin, $dbSenha, $dbNomeBanco, $dbPorta);
+
 // importar as funções que vão ser utilizadas nas rotas
 include 'rotas.php';
 
@@ -16,6 +20,9 @@ switch ($url[0]) {
     notfound();
     break;
 }
+
+// fechar conexão com o banco de dados
+mysqli_close($con);
 
 // incluir o mesmo head em todas as rotas (basicamente o fechamento das tags html)
 include __DIR__ . '/includes/foot.php';
